73115466a909a7b94bfd1c18f94c6802b8370577
SkyGrel19 <skibav2002@gmail.com>
2024-08-08 11:07:21 +0300

    rewrite to criterion framework

--- benches/rw.rs
+++ benches/rw.rs
@@ -1,157 +1,129 @@
-#![feature(test)]
-
-extern crate test;
-
 use std::cell::RefCell;
 use std::sync::atomic::{AtomicUsize, Ordering};
-use test::{Bencher, black_box};
+use criterion::{black_box, Criterion, criterion_group, criterion_main};
 
+const SIZE: usize = 1_000;
 
-const SIZE: usize = 1_000_000;
-
-
-
-#[bench]
-fn test_array_read(b: &mut Bencher) {
+fn test_array_read(c: &mut Criterion) {
     let mut vec: Vec<usize> = (0..SIZE).map(|i| black_box(i)).collect();
 
     let mut sum = 0;
-    b.iter(|| {
-        for i in 0..SIZE {
-            sum += black_box(vec[i]);
-        }
+    c.bench_function("test_array_read", |b| {
+        b.iter(|| {
+            for i in 0..SIZE {
+                sum += black_box(vec[i]);
+            }
+        });
     });
+    // c.bench_function("test_array_read_no_blackbox", |b| {
+    //     b.iter(|| {
+    //         for i in 0..SIZE {
+    //             sum += vec[i];
+    //         }
+    //     });
+    // });
     println!("Sum is {sum}!");
 }
 
-#[bench]
-fn test_array_read_no_blackbox(b: &mut Bencher) {
-    let mut vec: Vec<usize> = (0..SIZE).map(|i| i).collect();
-
-    let mut sum = 0;
-    b.iter(|| {
-        for i in 0..SIZE {
-            sum += vec[i];
-        }
-    });
-    println!("Sum is {sum}!");
-}
-
-#[bench]
-fn test_array_read_atomic(b: &mut Bencher) {
+fn test_array_read_atomic(c: &mut Criterion) {
     let mut vec: Vec<AtomicUsize> = (0..SIZE).map(|i| black_box(AtomicUsize::new(i))).collect();
 
     let mut sum = 0;
-    b.iter(|| {
-        for i in 0..SIZE {
-            sum += black_box(vec[i].load(Ordering::Relaxed));
-        }
+    c.bench_function("test_array_read_atomic", |b| {
+        b.iter(|| {
+            for i in 0..SIZE {
+                sum += black_box(vec[i].load(Ordering::Relaxed));
+            }
+        });
     });
+    // c.bench_function("test_array_read_atomic_no_blackbox", |b| {
+    //     b.iter(|| {
+    //         for i in 0..SIZE {
+    //             sum += vec[i].load(Ordering::Relaxed);
+    //         }
+    //     });
+    // });
     println!("Sum is {sum}!");
 }
 
-#[bench]
-fn test_array_read_atomic_no_blackbox(b: &mut Bencher) {
-    let mut vec: Vec<AtomicUsize> = (0..SIZE).map(|i| AtomicUsize::new(i)).collect();
-
-    let mut sum = 0;
-    b.iter(|| {
-        for i in 0..SIZE {
-            sum += vec[i].load(Ordering::Relaxed);
-        }
-    });
-    println!("Sum is {sum}!");
-}
-
-#[bench]
-fn test_array_read_refcell(b: &mut Bencher) {
+fn test_array_read_refcell(c: &mut Criterion) {
     let mut vec: Vec<RefCell<usize>> = (0..SIZE).map(|i| black_box(RefCell::new(i))).collect();
 
     let mut sum = 0;
-    b.iter(|| {
-        for i in 0..SIZE {
-            sum += black_box(*vec[i].borrow());
-        }
+    c.bench_function("test_array_read_refcell", |b| {
+        b.iter(|| {
+            for i in 0..SIZE {
+                sum += black_box(*vec[i].borrow());
+            }
+        });
     });
-    println!("Sum is {sum}!");
-}
-
-#[bench]
-fn test_array_read_refcell_no_blackbox(b: &mut Bencher) {
-    let mut vec: Vec<RefCell<usize>> = (0..SIZE).map(|i| RefCell::new(i)).collect();
-
-    let mut sum = 0;
-    b.iter(|| {
-        for i in 0..SIZE {
-            sum += *vec[i].borrow();
-        }
-    });
+    // c.bench_function("test_array_read_refcell_no_blackbox", |b| {
+    //     b.iter(|| {
+    //         for i in 0..SIZE {
+    //             sum += *vec[i].borrow();
+    //         }
+    //     });
+    // });
     println!("Sum is {sum}!");
 }
 
 
-#[bench]
-fn test_array_write(b: &mut Bencher) {
+fn test_array_write(c: &mut Criterion) {
     let mut vec: Vec<usize> = (0..SIZE).map(|i| black_box(i)).collect();
 
-    b.iter(|| {
-        for i in 0..SIZE {
-            vec[i] = black_box(15);
-        }
+    c.bench_function("test_array_write", |b| {
+        b.iter(|| {
+            for i in 0..SIZE {
+                vec[i] = black_box(15);
+            }
+        });
     });
+    // c.bench_function("test_array_write_no_blackbox", |b| {
+    //     b.iter(|| {
+    //         for i in 0..SIZE {
+    //             vec[i] = 15;
+    //         }
+    //     });
+    // });
 }
 
-#[bench]
-fn test_array_write_no_blackbox(b: &mut Bencher) {
-    let mut vec: Vec<usize> = (0..SIZE).map(|i| i).collect();
-
-    b.iter(|| {
-        for i in 0..SIZE {
-            vec[i] = 15;
-        }
-    });
-}
-
-#[bench]
-fn test_array_write_atomic(b: &mut Bencher) {
+fn test_array_write_atomic(c: &mut Criterion) {
     let mut vec: Vec<AtomicUsize> = (0..SIZE).map(|i| black_box(AtomicUsize::new(i))).collect();
 
-    b.iter(|| {
-        for i in 0..SIZE {
-            vec[i].store(black_box(15), Ordering::Relaxed);
-        }
+    c.bench_function("test_array_write_atomic", |b| {
+        b.iter(|| {
+            for i in 0..SIZE {
+                vec[i].store(black_box(15), Ordering::Relaxed);
+            }
+        });
     });
+    // c.bench_function("test_array_write_atomic_no_blackbox", |b| {
+    //     b.iter(|| {
+    //         for i in 0..SIZE {
+    //             vec[i].store(15, Ordering::Relaxed);
+    //         }
+    //     });
+    // });
 }
 
-#[bench]
-fn test_array_write_atomic_no_blackbox(b: &mut Bencher) {
-    let mut vec: Vec<AtomicUsize> = (0..SIZE).map(|i| AtomicUsize::new(i)).collect();
-
-    b.iter(|| {
-        for i in 0..SIZE {
-            vec[i].store(15, Ordering::Relaxed);
-        }
-    });
-}
-
-#[bench]
-fn test_array_write_refcell(b: &mut Bencher) {
+fn test_array_write_refcell(c: &mut Criterion) {
     let mut vec: Vec<RefCell<usize>> = (0..SIZE).map(|i| black_box(RefCell::new(i))).collect();
 
-    b.iter(|| {
-        for i in 0..SIZE {
-            *vec[i].borrow_mut() = black_box(15);
-        }
+    c.bench_function("test_array_write_refcell", |b| {
+        b.iter(|| {
+            for i in 0..SIZE {
+                *vec[i].borrow_mut() = black_box(15);
+            }
+        });
     });
+    // c.bench_function("test_array_write_refcell_no_blackbox", |b| {
+    //     b.iter(|| {
+    //         for i in 0..SIZE {
+    //             *vec[i].borrow_mut() = 15;
+    //         }
+    //     });
+    // });
 }
 
-#[bench]
-fn test_array_write_refcell_no_blackbox(b: &mut Bencher) {
-    let mut vec: Vec<RefCell<usize>> = (0..SIZE).map(|i| RefCell::new(i)).collect();
-
-    b.iter(|| {
-        for i in 0..SIZE {
-            *vec[i].borrow_mut() = 15;
-        }
-    });
-}
+criterion_group!(benches, test_array_read, test_array_read_atomic, test_array_read_refcell, test_array_write, test_array_write_atomic, test_array_write_refcell);
+criterion_main!(benches);
